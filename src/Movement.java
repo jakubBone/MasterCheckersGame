919fda3c50af59c1a8598a4d169a2d6bb8580bd4
jakubBone <jakub.bone@poczta.onet.pl>
2023-12-01 11:19:11 +0100

    Commit 21: Refactoring of capturePawn() in progress

--- src/Movement.java
+++ src/Movement.java
@@ -2,10 +2,9 @@ public class Movement {
     int movementRow; int movementColumn;
     int pawnRow; int pawnColumn;
     int moveLeft; int moveRight;
-    int rowAbove; int rowBelow;
-    int twoRowsAbove; int twoRowsBelow;
-    int transitionFieldX; int transitionFieldY;
-
+    int twoMovesLeft; int twoMovesRight;
+    int rowAbove; int twoRowsAbove;
+    int transitionColumnOnLeft; int transitionColumnOnRight;
 
     void performMove() {
         setMovementDetails();
@@ -29,14 +28,11 @@ public class Movement {
     }
 
     void setMovementDetails(){
-        moveLeft = pawnColumn - 1;
-        moveRight = pawnColumn + 1;
+        moveLeft = pawnColumn - 1; moveRight = pawnColumn + 1;
+        twoMovesLeft = pawnColumn - 2; twoMovesRight = pawnColumn + 2;
         rowAbove = pawnRow - 1;
-        rowBelow = pawnRow + 1;
         twoRowsAbove = pawnRow - 2;
-        twoRowsBelow = pawnRow + 2;
-        transitionFieldX = movementRow + 1;
-        transitionFieldY = movementColumn - 1;
+        transitionColumnOnLeft = pawnColumn  - 1 ; transitionColumnOnRight = pawnColumn + 1;
     }
 
     void printMessageOfInvalidMove(){
@@ -46,55 +42,54 @@ public class Movement {
             System.out.println("Only upward movement is allowed");
         else if (!(isEnemyOnTransition()))
             System.out.println("There is no enemy on transition field");
-        else if (!isMovementOutOfRange()) // Correct it!
-            System.out.println("The movement is out of valid range");
         else if(!(isMoveDiagonal()))
             System.out.println("The move is not diagonal");
     }
 
-    boolean isMovementValid() {
-        return (isMovementOutOfRange() && isMoveDiagonal() &&
-                isMovementUpward() && isSelectedFieldEmpty());
+    void jumpToField() {
+        Board.board[movementRow][movementColumn] = Player.playerPAWN;
+        Board.board[pawnRow][pawnColumn] = Board.emptyField;
     }
-    boolean isMovementOutOfRange(){
-        return (movementRow >= twoRowsAbove && movementRow <= twoRowsBelow);
+
+    boolean isMovementValid() {
+        return (isMovementUpward() && isMoveDiagonal()  && isSelectedFieldEmpty());
     }
 
     boolean isMoveDiagonal() {
         return (!(movementColumn == pawnColumn) && !(movementRow == pawnRow));
-    }
-    void jumpToField() {
-            Board.board[movementRow][movementColumn] = Player.playerPAWN;
-            Board.board[pawnRow][pawnColumn] = Board.emptyField;
     }
 
     boolean isMovementUpward(){
         return (isRowAboveSelected() || areTwoRowsAboveSelected());
     }
     boolean isRowAboveSelected() {
-        return (movementRow == rowAbove && (movementColumn == moveLeft || movementColumn == moveRight));
+        return (movementRow == rowAbove && isMoveDiagonal() &&
+                (movementColumn == moveLeft || movementColumn == moveRight));
     }
     boolean areTwoRowsAboveSelected() {
-        return (movementRow == twoRowsAbove);
+        return (movementRow == twoRowsAbove && isMoveDiagonal() &&
+                (movementColumn == twoMovesLeft || movementColumn == twoMovesRight));
     }
 
     boolean isSelectedFieldEmpty(){
         return (Board.board[movementRow][movementColumn] == Board.emptyField);
     }
     boolean isEnemyOnTransition() {
-        return (Board.board[transitionFieldX][transitionFieldY] == Player.computerPAWN);
+        return ((Board.board[rowAbove][transitionColumnOnLeft] == Player.computerPAWN) ||
+                (Board.board[rowAbove][transitionColumnOnRight] == Player.computerPAWN));
     }
 
     void capturePawn() {
         if(isSelectedFieldEmpty() && isEnemyOnTransition()){
-            Board.board[movementRow][movementColumn] = Player.playerPAWN;
-            Board.board[transitionFieldX][transitionFieldY] = Board.emptyField;
-            Board.board[pawnRow][pawnColumn] = Board.emptyField;
+            jumpToField();
+            if(Board.board[rowAbove][transitionColumnOnLeft] == Player.computerPAWN)
+                Board.board[rowAbove][transitionColumnOnLeft] = Board.emptyField;
+            else if (Board.board[rowAbove][transitionColumnOnRight] == Player.computerPAWN)
+                Board.board[rowAbove][transitionColumnOnRight] = Board.emptyField;
             Player.compPawnNumbers -= 1;
         }
-        else
-            System.out.println("There is no enemy in transition.5");
     }
+
 }
 
 
